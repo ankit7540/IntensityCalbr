7126df04b08a256bca76126677aa1df7e7bc00e9
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2021-06-28 01:35:45 +0800

    Update gen_correction.py

--- PythonModule/determine_C0_C1_correction/gen_correction.py
+++ PythonModule/determine_C0_C1_correction/gen_correction.py
@@ -1,2 +1,10 @@
 
 # This file has function(s) for determination of the C0 and C1 corrections
+
+
+
+
+# fit function for the white light
+
+def photons_per_unit_wavenum_abs(x,a,T) :
+    return (a*599584916*w**2)/(exp(0.1438776877e-1*x/T)-1)
