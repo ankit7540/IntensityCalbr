a2773a84fc570afe9df2a46ddbc7c9b747994b1c
ankit7540 <ankit7540.ipm02g@nctu.edu.tw>
2021-06-28 21:15:30 +0800

    functions for c0 and c1 corr

--- PythonModule/determine_C0_C1_correction/gen_correction.py
+++ PythonModule/determine_C0_C1_correction/gen_correction.py
@@ -1,10 +1,91 @@
+#############################################################################
+
+import numpy as np
+from numpy.polynomial import Polynomial
+import math
+import matplotlib.pyplot as plt
+
+#############################################################################
+
+axis=np.loadtxt('axis.txt')
+wl=np.loadtxt('wl.txt')
+
 
 # This file has function(s) for determination of the C0 and C1 corrections
 
+def gen_C0_C1 (Ramanshift, laser_nm, wl_spectra, norm_pnt, mask=None, set_mask_nan=None):
+
+    '''Ramanshift = vector, the x-axis in wavenumbers
+       laser_nm = scalar, the laser wavelength in nanometers,
+       wl_spectra = broadband whitelight spectra (1D or 2D),
+       norm_pnt =  normalization point (corrections will be set
+                                        to unity at this point),
+
+       OPTIONAL PARAMETERS
+       mask = vector, mask wave for selecting specific region to fit ,
+       set_mask_nan= boolean, 1 will set the masked region in 
+       the output correction to
+          nan, 0 will not '''
+
+    
+    C0 = gen_C0 (Ramanshift, norm_pnt)
+    
+    # check the whitelight spectrum provided 
+    dim = wl_spectra.shape
+    if(dim[0] != Ramanshift.shape[0]):
+        print ("\t Error : Dimension mismatch for wl spectra and the xaxis")
+    
+    if (dim[1] > 1 and dim[1] ):
+        print ("\t wl spectra is 2D")
+        wl_spectra = np.mean(wl_spectra, axis=1)
+        print(wl_spectra.shape)
+        
+    # normalize the wl 
+    wl_norm = wl_spectra / np.amax(wl_spectra)
+    
+    # correct wl with C0
+    wl_norm_C0 = np.multiply(wl_norm , C0)
+    
+    # check if mask supplied
+    
+    if (mask==None):
+        print ("\t Mask not available. Proceeding with fit..")
+    else (isinstance(obj, numpy.ndarray) == 1):
+        print ("\t Mask is available.")
+        
+
+    #----------------------------------------------------------
 
 
+#############################################################################
 
-# fit function for the white light
+def gen_C0 (Ramanshift, norm_pnt):
+    '''Ramanshift = vector, the x-axis in wavenumbers
+       norm_pnt =  normalization point (corrections will be set 
+                                        to unity at this point) '''
+
+    nPnts = Ramanshift.shape[0]
+    
+    
+    spacing = np.diff(Ramanshift)
+
+    # normalization 
+    wavenum_corr  = spacing / spacing [norm_pnt]
+    temp_xaxis = np.arange(spacing.shape[0])
+
+    
+    # fitting the wavenumber diff
+    c = Polynomial.fit(temp_xaxis , wavenum_corr, deg=3)
+
+    # extrpolate the last point    
+    wavenum_corr = np.append(wavenum_corr, c(spacing.shape[0]+1))
+    
+    return wavenum_corr
+    
+
+#############################################################################
 
 def photons_per_unit_wavenum_abs(x,a,T) :
-    return (a*599584916*w**2)/(exp(0.1438776877e-1*x/T)-1)
+    return (a*599584916*x**2)/(math.exp(0.1438776877e-1*x/T)-1)
+
+#############################################################################
